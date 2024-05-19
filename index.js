95ec65b8884d647d25b0c94d50331022d2739f25
Jagan0901 <110884222+Jagan0901@users.noreply.github.com>
2024-05-19 23:37:41 +0530

    added changes

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post("/upload", upload.single("file"), (req, res) => {
   res.status(201).send("File uploaded");
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
